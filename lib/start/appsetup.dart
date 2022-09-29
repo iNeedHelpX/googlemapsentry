6de470da2d37e5adf0a5e984f7a2a0918e9aeeb4
Julia Pak <jpak0444@gmail.com>
2022-09-29 15:30:29 -0400

    pages

--- lib/start/appsetup.dart
+++ lib/start/appsetup.dart
@@ -1,6 +1,9 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:googlemapsentry/colors/colours_list.dart';
+import 'package:googlemapsentry/pages/account_page.dart';
+import 'package:googlemapsentry/pages/home_page.dart';
+import 'package:googlemapsentry/pages/map_page.dart';
 import 'package:googlemapsentry/start/app_bars/listbaricons.dart';
 import 'package:googlemapsentry/start/app_bars/radialbar.dart';
 
@@ -14,10 +17,11 @@ class AppSetup extends StatefulWidget {
 class _AppSetupState extends State<AppSetup> {
   int selectedpage = 0;
 
-  final _pageOptions = [
+  final _pageOptions = const [
 //pages
-
-    //settings or acc icon
+    HomePage(),
+    MapPage(),
+    AccountPage(),
   ];
 
   CurvedNavigationBar curvedNavBar() {
