eb8288ca92d3ef7c4ab74be820f01463c71bbb20
Julia Pak <jpak0444@gmail.com>
2022-09-30 18:09:52 -0400

    cart

---
+++ lib/controllers/cart_controller.dart
@@ -0,0 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:googlemapsentry/globalvars.dart';
+import 'package:googlemapsentry/models/user_model.dart';
+import 'package:uuid/uuid.dart';
+
+class CartController extends GetxController {
+  static CartController instance = Get.find();
+  //cart controller for the shopping cart
+
+}
