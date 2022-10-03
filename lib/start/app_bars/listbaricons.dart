be36d7c85a06d50656914f5ac00cc7c4f37a3edd
Julia Pak <jpak0444@gmail.com>
2022-10-02 23:46:47 -0400

    icons

--- lib/start/app_bars/listbaricons.dart
+++ lib/start/app_bars/listbaricons.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:googlemapsentry/icons/account_icon.dart';
+import 'package:googlemapsentry/icons/cup_icon.dart';
 import 'package:googlemapsentry/icons/map_icon.dart';
 import 'package:googlemapsentry/icons/panda_icon.dart';
 
 List<Widget> listBarIcons() {
   return const <Widget>[
     //icons list here
-    PandaIcn(),
+    CupIcon(),
     MapIcn(),
     AccIcn(),
   ];
